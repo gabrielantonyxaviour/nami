ecd75d8ccc33820e00911e1894b63388a8a86c4a
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2024-12-03 02:14:15 +0530

    cooking

--- nami-xmtp-bot/src/index.ts
+++ nami-xmtp-bot/src/index.ts
@@ -1,17 +1,21 @@
-import {
-  run,
-  agentReply,
-  replaceVariables,
-  XMTPContext,
-} from "@xmtp/message-kit";
+import { createBrianAgent } from "@brian-ai/langchain";
+import { ChatOpenAI } from "@langchain/openai";
+import { run, HandlerContext } from "@xmtp/message-kit";
 
-import { systemPrompt } from "./prompt.js";
-
-run(async (context: XMTPContext) => {
-  const {
-    message: { sender },
-  } = context;
-
-  let prompt = await replaceVariables(systemPrompt, sender.address, [], "@bot");
-  await agentReply(context, prompt);
+run(async (context: HandlerContext) => {
+  const brianAgent = await createBrianAgent({
+    apiKey: process.env.BRIAN_API_KEY!,
+    privateKeyOrAccount: process.env.KEY as `0x${string}`,
+    llm: new ChatOpenAI({ apiKey: process.env.OPENAI_API_KEY }),
+    instructions: "You're a web3 assistant. You speak like a wookie.",
+    xmtpHandler: context,
+    xmtpHandlerOptions: {
+      onAgentAction: true,
+      onToolStart: true,
+      onToolError: true,
+    },
+  });
+  const { content } = context.message;
+  const result = await brianAgent.invoke({ input: content.text });
+  await context.send(result.output);
 });
